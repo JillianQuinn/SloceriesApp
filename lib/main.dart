f87f472aa440884454fa67bee3ef24161e280fbb
Cole Perry <coleperry727@Coles-MacBook-Pro-2.local>
2019-11-16 15:08:26 -0800

    Added Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'home_widget.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'SLOCERIES'),
+      home: Home(),
     );
   }
 }
