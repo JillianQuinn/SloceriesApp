113a37f23d088c00426718fa2853f8283cf0209e
Jillian Quinn <Quinn@Jillian-Quinns-MacBook.local>
2019-11-16 17:22:17 -0800

    added form

--- lib/placeholder_widget.dart
+++ lib/placeholder_widget.dart
@@ -43,6 +43,7 @@ class ProfileWidget extends StatelessWidget {
 }
 
 class AddWidget extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,6 +51,53 @@ class AddWidget extends StatelessWidget {
         title: Text('Add a Trip'),
       ),
       body: Center(
+        child: new Container(
+            padding: new EdgeInsets.all(20.0),
+            child: new Form(
+              key: this._formKey,
+              child: new ListView(
+                children: <Widget>[
+                  new TextFormField(
+                      keyboardType: TextInputType.text, // Use email input type for emails.
+                      decoration: new InputDecoration(
+                          hintText: 'Firstname Lastname',
+                          labelText: 'Name'
+                      ),
+                      onSaved: (String value)
+                      {
+                        //this._data.email = value;
+                      }
+                  ),
+                  new TextFormField(
+                      keyboardType: TextInputType.number, // Use secure text for passwords.
+                      decoration: new InputDecoration(
+                          hintText: 'ex: \$10',
+                          labelText: 'Price: '
+                      ),
+                      onSaved: (String value) {
+                        //this._data.password = value;
+                      }
+                  ),
+
+                  new Container(
+                    child: new RaisedButton(
+                      child: new Text(
+                        'Post',
+                        style: new TextStyle(
+                            color: Colors.white
+                        ),
+                      ),
+                      //onPressed: this.submit,
+                      color: Colors.blue,
+                    ),
+                    margin: new EdgeInsets.only(
+                        top: 20.0
+                    ),
+                  )
+                ],
+              ),
+            )
+        ),
         //Add Code Here
       ),
     );
